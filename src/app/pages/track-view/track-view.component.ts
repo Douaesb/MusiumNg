aefcab808b5ccafd532609dbe2f9a02719f6a553
Douae sb <douae.sb411@gmail.com>
2024-12-31 15:22:45 +0100

    MS-2-Project-Structure

---
+++ src/app/pages/track-view/track-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './track-view.component.html',
+  styleUrl: './track-view.component.scss'
+})
+export class TrackViewComponent {
+
+}
